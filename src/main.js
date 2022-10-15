38c1d0b9e38db3c706fe29e7f10dcd0a17b028e7
qmedina <quetz_medina@brown.edu>
2022-10-15 15:53:28 -0400

    daddy swishtian another 3

--- src/main.js
+++ src/main.js
@@ -20,6 +20,8 @@ $(document).ready(function() {
       x: -1,
       y: -1
     };
+
+    var tagged = false;
   
     function mouseMoveHandler(event) {
       currentMousePos.x = event.pageX;
@@ -27,27 +29,41 @@ $(document).ready(function() {
     }
 
     function mouseOverHandler(event){
-        document.getElementById("cube").style.background = "blue";
-    }
-
-    function mouseOutHandler(event){
-        document.getElementById("cube").style.background = "red";
+        if (tagged){
+            document.getElementById("cube").style.background = "red";
+            document.getElementById("title").innerHTML = "Run!";
+        }
+        else {
+            document.getElementById("cube").style.background = "blue";
+            document.getElementById("title").innerHTML = "Tag! You're it!";
+        }
+        tagged = !tagged;
     }
   
     mouseMover = setInterval(positionUpdate, 20);
   
     function positionUpdate() {
-  
-      var x_cursor = currentMousePos.x;
-      var y_cursor = currentMousePos.y;
-      var position = $("#cube").offset();
-      var x_box = position.left;
-      var y_box = position.top;
-  ``
-      $("#cube").animate({
-        left: x_box + 0.1 * (x_cursor - x_box),
-        top: y_box + 0.1 * (y_cursor - y_box)
-      }, 1, "linear");
+        if (tagged == false) {
+            var x_cursor = currentMousePos.x;
+            var y_cursor = currentMousePos.y;
+            var position = $("#cube").offset();
+            var x_box = position.left;
+            var y_box = position.top;
+            $("#cube").animate({
+                left: x_box + 0.1 * (x_cursor - x_box),
+                top: y_box + 0.1 * (y_cursor - y_box)
+            }, 1, "linear");
+        } else {
+            var x_cursor = currentMousePos.x;
+            var y_cursor = currentMousePos.y;
+            var position = $("#cube").offset();
+            var x_box = position.left;
+            var y_box = position.top;
+            $("#cube").animate({
+                left: x_box - 0.1 * (x_cursor - x_box),
+                top: y_box - 0.1 * (y_cursor - y_box)
+            }, 1, "linear");
+        } 
     }
   });
   
