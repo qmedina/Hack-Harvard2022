c9200a47d2077da2bae4258411b4ae29538ba7d2
qmedina <quetz_medina@brown.edu>
2022-10-15 22:35:41 -0400

    u hot

--- src/main.js
+++ src/main.js
@@ -22,6 +22,19 @@ $(document).ready(function() {
     };
 
     var tagged = false;
+
+    console.log(window.innerHeight);
+    console.log(window.innerWidth);
+
+    function isInViewport(element) {
+        console.log("ran isInViewport");
+        return (
+            element.top >= 120 &&
+            element.left >= 120 &&
+            window.innerHeight - element.top >=  120 &&
+            window.innerWidth - element.left >= 120 
+        )
+    }
   
     function mouseMoveHandler(event) {
       currentMousePos.x = event.pageX;
@@ -40,29 +53,31 @@ $(document).ready(function() {
         tagged = !tagged;
     }
   
-    mouseMover = setInterval(positionUpdate, 20);
+    setInterval(positionUpdate, 20);
   
     function positionUpdate() {
-        if (tagged == false) {
-            var x_cursor = currentMousePos.x;
+        var x_cursor = currentMousePos.x;
             var y_cursor = currentMousePos.y;
             var position = $("#cube").offset();
             var x_box = position.left;
             var y_box = position.top;
+
+        if (tagged == false) {
             $("#cube").animate({
                 left: x_box + 0.1 * (x_cursor - x_box),
                 top: y_box + 0.1 * (y_cursor - y_box)
             }, 1, "linear");
         } else {
-            var x_cursor = currentMousePos.x;
-            var y_cursor = currentMousePos.y;
-            var position = $("#cube").offset();
-            var x_box = position.left;
-            var y_box = position.top;
-            $("#cube").animate({
-                left: x_box - 0.1 * (x_cursor - x_box),
-                top: y_box - 0.1 * (y_cursor - y_box)
-            }, 1, "linear");
+            if (isInViewport(position)) {
+                console.log("isInViewport");
+                $("#cube").animate({
+                    left: x_box - 0.055 * (x_cursor - x_box),
+                    top: y_box - 0.055 * (y_cursor - y_box)
+                }, 1, "linear");
+            } else {
+                console.log("isnooooootInViewport");
+                document.getElementById("body").style.background = "red";
+            } 
         } 
     }
   });
